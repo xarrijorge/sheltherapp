0f5badee9e8ebaa46ac081232004eda88ec2d9d6
Xarri Jorge <kodinglife@gmail.com>
2024-08-28 21:14:53 +0300

    Minor edit to places component

--- src/screens/Places.js
+++ src/screens/Places.js
@@ -6,6 +6,23 @@ const PlacesScreen = () => {
     return (
         <View style={styles.container}>
             <Text>Places Screen</Text>
+              {/* Modal for selecting places */}
+          {/* Modal for selecting places */}
+            {/* <Modal
+                visible={placeModalVisible}
+                animationType="slide"
+                transparent={true}
+                onRequestClose={() => setPlaceModalVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <Button onPress={() => setPlaceModalVisible(false)}> 
+                        <Text style={styles.modalTitle}>Close</Text> </Button>
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalTitle}>Search for a Place</Text>
+                        <SelectPlace/>
+                    </View>
+                </View>
+            </Modal>  */}
         </View>
     );
 };
