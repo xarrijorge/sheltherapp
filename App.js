86d515ad3722cdbf89b9ced050038935d2b70eef
Xarri Jorge <kodinglife@gmail.com>
2024-08-28 22:08:40 +0300

    Created shelther folder on startup

--- App.js
+++ App.js
@@ -3,12 +3,34 @@ import AppNavigator from './src/navigator/AppNavigator';
 import { LoadingProvider, useLoading } from './src/utils/loadingContext';
 import { setupAxiosInterceptors } from './src/utils/axiosConfig';
 import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
 const App = () => {
   const { isLoading, setIsLoading } = useLoading();
 
+  const SHELTER_DIRECTORY = `${FileSystem.documentDirectory}.shelter/`; // Hidden directory
+
   useEffect(() => {
     setupAxiosInterceptors(setIsLoading);
+
+    const createShelterDirectory = async () => {
+      try {
+        // Check if the directory already exists
+        const dirInfo = await FileSystem.getInfoAsync(SHELTER_DIRECTORY);
+        
+        if (!dirInfo.exists) {
+          // If the directory doesn't exist, create it
+          await FileSystem.makeDirectoryAsync(SHELTER_DIRECTORY, { intermediates: true });
+          console.log('Shelter directory created');
+        } else {
+          console.log('Shelter directory already exists');
+        }
+      } catch (error) {
+        console.error('Error creating shelter directory:', error);
+      }
+    };
+
+    createShelterDirectory();
   }, [setIsLoading]);
 
   return (
